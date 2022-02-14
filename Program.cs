ae5cf6ede573dcdb137e5b470d6dc83b710ee410
naufalnny <naufalfarras117@gmail.com>
2022-02-14 17:00:51 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void sort(int[] number){
+        for (int i = 0;i <number.Count(); i++)
+        {
+            int final = number[i];
+            int idx = i;
+
+            while (idx>0 && number[idx-1] > final)
+            {
+                number[idx] = number[idx-1];
+                idx--;
+            }
+            number[idx] = final;
+
+        }
+
+            
+        }
+        static void Main(string[] args)
+        {
+        int[] angka = {24,12,36,70,23};
+        Console.WriteLine("Array Sebelum =");
+            for (int i = 0; i < angka.Count(); i++)
+            {
+                Console.Write(angka[i]+ " ");
+            }
+        Console.WriteLine(" ");
+        sort(angka);
+        Console.WriteLine("Array Sesudah =");
+        for(int i = 0;i < angka.Count(); i++){
+            Console.Write(angka[i]+ " ");
+        }
+        }
+    }
+}
+
